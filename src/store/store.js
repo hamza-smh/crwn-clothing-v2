07262b2c331fcee12f86b94e4e0c1d05c787c091
hamza-smh <hamza.smh2000@gmail.com>
2023-03-22 19:46:58 +0500

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -15,7 +15,7 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log('Type: ',action.type);
     console.log('Payload: ', action.payload);
     console.log('Current state: ', store.getState());
-
+// console.log (Useless comment);
     next(action);
     console.log('New state: ', store.getState());
 }
