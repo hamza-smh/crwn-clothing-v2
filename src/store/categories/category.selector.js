8c5d8c2bc47ea7022f3a89f61d9fdcf268dbca7c
hamza-smh <hamza.smh2000@gmail.com>
2023-03-24 16:27:28 +0500

    Category Selector to Memoization Function

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,12 +1,51 @@
-export const selectCategoriesMap = (state) =>{
+//   * Doesn't render until the [] value is changed imporves data cycle
 
-    console.log("Selector Fired");
-    return (
-        state.categories.categories.reduce((acc, category) => {
-                const {title,items} = category;
-                acc[title.toLowerCase()]=items;
-            return acc;
-        },
-    {}
-    ))
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => {
+    console.log("Selector 1 Fired")
+    return state.categories;
 }
+
+//This is a memoized selector
+export const selectCategories = createSelector(
+    [selectCategoryReducer],                                    //input selector
+    (categoriesSlice) => {                                        //output selector*
+        console.log("Selector 2 Fired")
+    return categoriesSlice.categories               
+});
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],                                   //input selectorMap
+    (categories) => {                                           //output selectorMap*       
+             console.log("Selector 3 Fired");                                                        
+        return  categories.reduce((acc, category) => {
+                    const {title,items} = category;
+                    acc[title.toLowerCase()] = items;
+                return acc;
+        }, {});
+    }
+);
+
+
+
+
+
+
+
+
+
+
+
+// export const selectCategoriesMap = (state) =>{
+
+//     console.log("Selector Fired");
+//     return (
+//         state.categories.categories.reduce((acc, category) => {
+//                 const {title,items} = category;
+//                 acc[title.toLowerCase()]=items;
+//             return acc;
+//         },
+//     {}
+//     ))
+// }
